78c0430f768d7088be13f5d01a9ed49646e7cde5
NataliiaBumbu <130250304+NataliiaBumbu@users.noreply.github.com>
2024-10-01 16:50:43 +0300

    hr

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -75,6 +75,7 @@ const MovieDetails = () => {
         <li><Link to="cast">Cast</Link></li>
         <li><Link to="reviews">Reviews</Link></li>
       </ul>
+      <hr />
     </div>
   )}
 </section>
